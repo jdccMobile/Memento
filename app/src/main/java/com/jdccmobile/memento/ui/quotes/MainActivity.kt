8e96eeeb858ae35af30bb74e444f95a7ce67c431
Jose <josedcastro97@gmail.com>
2023-04-05 12:19:07 +0200

    mvvm and dagger hilt implemented

--- app/src/main/java/com/jdccmobile/memento/ui/quotes/MainActivity.kt
+++ app/src/main/java/com/jdccmobile/memento/ui/quotes/MainActivity.kt
@@ -2,16 +2,22 @@ package com.jdccmobile.memento.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.jdccmobile.memento.R
 import com.jdccmobile.memento.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
 }
 
