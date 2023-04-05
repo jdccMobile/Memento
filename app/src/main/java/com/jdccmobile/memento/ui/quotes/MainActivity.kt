ed4b9b8085480c2b76efb5cfee088a0f0abf958f
Jose <josedcastro97@gmail.com>
2023-04-05 09:54:21 +0200

    Initial commit

---
+++ app/src/main/java/com/jdccmobile/memento/ui/quotes/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.jdccmobile.memento.ui.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jdccmobile.memento.R
+
+// TODO VIEW BINDING, CONECTAR A FIREBASE, MVVM
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
