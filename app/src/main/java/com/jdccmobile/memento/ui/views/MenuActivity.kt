68352b9b87d92a9e31a17f7106566b3f1a580b7d
Jose <josedcastro97@gmail.com>
2023-04-10 11:57:13 +0200

    menu activity created

--- app/src/main/java/com/jdccmobile/memento/ui/views/MenuActivity.kt
+++ app/src/main/java/com/jdccmobile/memento/ui/views/MenuActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jdccmobile.memento.R
 
+
+//todo cambiar fuentes de los textos+
+//todo convertir cardview de estoicas clicable
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
